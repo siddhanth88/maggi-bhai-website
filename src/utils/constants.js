4dba338a010e2ce2e6c341dcb2ad10ef9b54fce3
SIDDHANTH REMMA <140525821+siddhanth88@users.noreply.github.com>
2026-01-09 15:05:05 +0530

    changed whatsupp number

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const whatsappPhoneNumber = "919022333333";
+export const whatsappPhoneNumber = "+91 97638 06250";
 export const whatsappSupportMessage = ` I stand with Maggi Bhai in building a stronger, safer, and more prosperous Bhiwandi. \nWard Number 9 deserves honest leadership and real development.\nWe want Maggi Bhai to win this election. Let’s bring positive change together.\nमैं मैगी भाई के साथ मिलकर भिवंडी को और अधिक मजबूत, सुरक्षित और समृद्ध बनाने के लिए प्रतिबद्ध हूँ।
 वार्ड नंबर 9 ईमानदार नेतृत्व और वास्तविक विकास का हकदार है।
 हम चाहते हैं कि मैगी भाई यह चुनाव जीतें। आइए मिलकर सकारात्मक बदलाव लाएँ।`;
